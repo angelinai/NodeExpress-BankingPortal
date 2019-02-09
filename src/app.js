3d57855938f533dc46dc1a0db750f6094bf9f2a6
Angelina Ilieva <kolosi98@hotmail.com>
2019-02-09 08:50:19 -0700

    Add profile view, account vew update

--- src/app.js
+++ src/app.js
@@ -9,12 +9,48 @@ const app = express();
 // configure where views can be found
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); // set view engine
-//app.engine('html', require('ejs').renderFile);
-//app.set('view engine', 'html');
-
+ 
 // point at the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index Page'}));
+// Load mock data from accounts.js, load here
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+)
+// Load mock data from users.js, load here
+const userData = fs.readFileSync(
+    path.join(__dirname, 'json', 'users.json'), 'utf8'
+)
+// Parese mock data 
+const accounts = JSON.parse(accountData);
+const users = JSON.parse(userData);
 
-app.listen(3000, () => console.log('Project running on port 3000!'))
+///////////////////////
+///     Routing    ///
+//////////////////////
+app.get('/', (req, res) =>  {
+    res.render('index', {   title: 'Account Summary', accounts });
+});
+
+app.get('/savings', (req, res) => {
+    res.render('account',  { account: accounts.savings });
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account',  { account: accounts.checking });
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account',  { account: accounts.credit });
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile',  { user: user[0] });
+});
+
+///////////////////////
+/// Server setup  ////
+//////////////////////
+app.listen(3000, () => {
+    console.log('Project running on port 3000!')
+});
