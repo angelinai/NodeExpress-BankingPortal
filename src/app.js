43aed71641414b656755e321cc636a04bc21ccb3
Angelina Ilieva <kolosi98@hotmail.com>
2019-02-10 09:36:49 -0700

    Add new get/post routes, update transfer and accounts view to support transfer and payment

--- src/app.js
+++ src/app.js
@@ -9,9 +9,16 @@ const app = express();
 // configure where views can be found
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); // set view engine
- 
+
+
+///////////////////////
+///   Middleware    ///
+//////////////////////
 // point at the public directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }))
+
 
 // Load mock data from accounts.js, load here
 const accountData = fs.readFileSync(
@@ -28,25 +35,56 @@ const users = JSON.parse(userData);
 ///////////////////////
 ///     Routing    ///
 //////////////////////
-app.get('/', (req, res) =>  {
-    res.render('index', {   title: 'Account Summary', accounts });
-});
+    
+// GET routes
+    app.get('/', (req, res) =>  {
+        res.render('index', {   title: 'Account Summary', accounts });
+    });
 
-app.get('/savings', (req, res) => {
-    res.render('account',  { account: accounts.savings });
-});
+    app.get('/savings', (req, res) => {
+        res.render('account',  { account: accounts.savings });
+    });
 
-app.get('/checking', (req, res) => {
-    res.render('account',  { account: accounts.checking });
-});
+    app.get('/checking', (req, res) => {
+        res.render('account',  { account: accounts.checking });
+    });
 
-app.get('/credit', (req, res) => {
-    res.render('account',  { account: accounts.credit });
-});
+    app.get('/credit', (req, res) => {
+        res.render('account',  { account: accounts.credit });
+    });
 
-app.get('/profile', (req, res) => {
-    res.render('profile',  { user: users[0] });
-});
+    app.get('/profile', (req, res) => {
+        res.render('profile',  { user: users[0] });
+    });
+
+    app.get('/transfer', (req, res) =>  {
+        res.render('transfer');
+    });
+    
+    app.get('/payment', (req, res) =>  {
+        res.render('payment',  { account: accounts.credit });
+    });
+ 
+// POST routes
+    app.post('/transfer', (req, res) =>  {
+       accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+       accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + 
+        parseInt(req.body.amount, 10);
+         
+        const accountsJson = JSON.stringify(accounts, null, 4);
+        // write to file
+        fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJson, 'utf8');
+      res.render('transfer', {message: "Transfer Completed"});
+    });
+    app.post('/payment', (req, res) =>  {
+        accounts.credit.balance -= req.body.amount;
+        accounts.credit.available += parseInt(req.body.account, 10);
+        const accountsJson = JSON.stringify(accounts, null, 4);
+
+         // write to file
+         fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJson, 'utf8');
+         res.render('payment', {message: 'Payment Sucessful', account: accounts.credit });
+    });
 
 ///////////////////////
 /// Server setup  ////
